73c7a56128cf24805d19b63080078b5c9b280e19
Rafael Pinho <89493675+RafaelPinho03@users.noreply.github.com>
2022-10-01 00:01:05 -0300

    Add files via upload

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,9 +1,9 @@
-def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+def maximizar_troca_de_figurinhas(self, figurinhas_da_maria, figurinhas_do_joao):
 
-
+    if figurinhas_da_maria != figurinhas_da_joao:
+        return figurinhas_da_joao
+    else: figurinhas_da_joao != figurinhas_da_maria
+    return figurinhas_da_maria
 
 if __name__ == '__main__':
     A, B = input().split(' ')
